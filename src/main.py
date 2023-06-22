c355bef9967308dccdc4eb6956b15a7bdfe89bc7
goose.mp4 <goosegoose.mp4@gmail.com>
2023-06-22 18:40:01 -0400

    Updated syntax for TODO list and updated main function
    Added a 'ruleset' to follow for the TODO list in the doc-string for easier navigation of things needing to be done and who's working on what.

--- src/main.py
+++ src/main.py
@@ -11,96 +11,40 @@ _desc         = "Multi-token discord bot control panel and manager"
 
 
 """
+TODO:
+
+-  : incomplete or to be added
+i- : currently being worked on by 'i' (initial)
+
 - Add logging
-- Start console terminal that shows all bots (display names) and their online status
+
 - Have user enter commands
-- If bot is defaulted to online include it in the loading print screen and actually do the start function for those bots.
+
+g- If bot is defaulted to online include it in the loading print screen and actually do the start function for those bots.
+
 - When user enters in the command to start/stop a bot, run that command that does that for that process
+
 - If user asks for info about a bot, clear the terminal and load the information via JSON and display it on screen, type 'back' to go back to the main menu
+
 - ONLY ADD DYNAMIC BOT ADDING IN QT DESIGNER VERSION, THIS IS JUST FOR PRODUCTION PURPOSES
+
+g/ Decided making the configuration for the actual control panel itself to be a txt file. Essentially the user opens the file and there will be things like '*USER_DISPLAY_NAME=your-name-here' so that we don't have to implement a specialized way to handle console commands that configure to control panel itself (plus I feel as if it's more user friendly as it seperates everything into seperate pieces/modules :-] )
 """
 
 # i am here -martin
-
-class DiscordBot():
-	def __init__(self, display_name:str, online:bool, key:str, debug:bool, show_token:bool, start_file_dir:str):
-		
-		self.processes = {}
-
-		# Dynamically add attributes
-		self.display_name = display_name
-		self.online 	  = online
-		self.key		  = key
-		self.debug		  = debug
-		self.show_token	  = show_token
-		self.start_file_dir = start_file_dir
-
-
-
-	def start_bot(self):
-		if exists(self.start_file_dir):
-			Popen("python main.py", cwd=self.start_file_dir)
-		else:
-			print("Bot start file not available (incorrect directory)")
-
-
-
-def generate_all_bot_objects(return_type:str):
-	"""
-	Generates either a list or a dictionary with objects from the DiscordBot() class
-	\n\n
-	Acceptable return types are 'list' and 'dict'
-	"""
-	
-	bot_config = get_json("bot_config")
-
-	# Determine return type
-	if return_type.lower() == "dict":
-		bot_list = {}
-	elif return_type.lower() == "list":
-		bot_list = []
-	else:
-		return ValueError("Accepts either 'list' or 'dict' only")
-	
-	for bot in bot_config:
-			
-			# Object info
-			bot_display_name   = bot["display_name"]
-			bot_online 		   = bot["online"]
-			bot_key 		   = bot["key"]
-			bot_debug 		   = bot["debug"]
-			bot_show_token 	   = bot["show_token"]
-			bot_start_file_dir = bot["start_file_dir"]
-
-			# Create object
-			bot_obj = DiscordBot(
-				display_name   = bot_display_name,
-				online 		   = bot_online,
-				key 		   = bot_key,
-				debug 		   = bot_debug,
-				show_token 	   = bot_show_token,
-				start_file_dir = bot_start_file_dir
-			)
-
-			# Add object to list
-			if return_type.lower() == "dict":
-				bot_list[bot_key] = bot_obj
-			elif return_type.lower() == "list":
-				bot_list.append(bot_obj)
-
-	return bot_list
+# epic 😎 -goose
 
 
 
 def get_json(json_file_name:str):
 	"""Returns a specified JSON file"""
-	return load(open(f"AutoPilot/src/storage/json/{json_file_name}.json"))
+	return load(open(f"src/storage/json/{json_file_name}.json"))
 
 
 
 def set_json(json_file_name, new_json):
 	"""Sets a specified JSON file to be different dictionary"""
-	with open(f"AutoPilot/src/storage/json/{json_file_name}.json", "w") as json_file:
+	with open(f"AutoPilotsrc/storage/json/{json_file_name}.json", "w") as json_file:
 		try:
 			dump(new_json, json_file, indent=4)
 			return
@@ -108,8 +52,10 @@ def set_json(json_file_name, new_json):
 			print(f"[!] {e}")
 
 
+
 def set_bot_online():
 	pass
+
 
 
 def aupi_process_command(console_command:str):
@@ -122,8 +68,6 @@ def aupi_process_command(console_command:str):
 
 	# Have preset list of valid keys
 	all_command_keys = [
-		"help",   # Displays all commands and what they do,
-		"config"  # Allows you to edit your control panel's configuration
 		"set",    # Sets the value of an attribute for a bot
 		"copy",   # Copies the value of an attribute for a bot
 		"toggle", # Toggles a bot on or off
@@ -132,12 +76,14 @@ def aupi_process_command(console_command:str):
 	# Attempt to get each required segment
 	try:
 		cmd_key 	  = console_command[0] # Which command the user is trying to use
-		cmd_bot_index = console_command[1] # The index of where the bot is on the list
-		cmd_attribute = console_command[2] # The attribute in which the user is changing
-		cmd_value 	  = console_command[3] # The new value the selected attribute is being changed to
+		bot_key 	  = console_command[1] # The index of where the bot is on the list
+		bot_attribute = console_command[2] # The attribute in which the user is changing
+		bot_value 	  = console_command[3] # The new value the selected attribute is being changed to
 
-	except:
-		return ValueError("Invalid command passed through console")
+	except IndexError as e:
+		# re-word all of this better, im really high when writing this
+		# All 4 parts should be present so in the case any are missing via IndexError you know the user is a moron
+		return ValueError(f"Insufficient amount of commands passed through! Error: {e}") 
 
 
 	# Determine what bot to get off the list
@@ -157,7 +103,7 @@ def aupi_process_command(console_command:str):
 def aupi_main():
 	"""Start the AutoPilot console control panel"""
 
-	bot_list = generate_all_bot_objects("list")
+	bot_list = get_json("bot_config")
 
 	while True:
 		console_display = f"# {_program_name} | by {_author} #\n\n"
@@ -167,12 +113,12 @@ def aupi_main():
 		for bot in bot_list:
 			
 			# Get bot information
-			display_name = bot.display_name
-			online       = bot.online
-			key          = bot.key
-			debug        = bot.debug
-			show_token 	 = bot.show_token
-			token 		 = bot.token if show_token else "N/A"
+			display_name = bot["display_name"]
+			online       = bot["online"]
+			key          = bot["key"]
+			debug        = bot["debug"]
+			show_token 	 = bot["show_token"]
+			token 		 = bot["token"] if show_token else "N/A"
 			online_display = "online" if online else "offline"
 			
 			# Add bot info to the console display
@@ -193,7 +139,7 @@ def aupi_main():
 	# Get user commands
 		try:
 			# Have user enter in a command
-			aupi_process_command(input("command:  "))
+			aupi_process_command(input("command >: "))
 		except Exception as e:
 			print(f"[!] An error has occurred while processing your command...\n{e}")
 
